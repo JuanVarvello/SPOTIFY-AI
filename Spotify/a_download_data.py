7ca64582edc865d5cf4b75fe2440cf768ac56ae8
JuanVarvello <118789279+JuanVarvello@users.noreply.github.com>
2023-04-04 08:34:35 -0300

    Add files via upload

---
+++ Spotify/a_download_data.py
@@ -0,0 +1,122 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+
+# Spotify Developer Account Credentials
+cid = '9dea5ea9eff74411a9c45db73d1ba08b'
+secret = '2e47c140ad124adeb623e15ce0c2a6bd'
+
+# Number columns, used for the recommender
+number_cols = ['valence', 'acousticness', 'danceability', 'duration_ms', 'energy',
+               'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo']
+
+
+def convert_json_into_df(results, sp):
+    """
+    Converts the downloaded results json from Spotify into a DataFrame
+
+    Parameters
+    ----------
+    results: dict
+        Dictionary containing song information from a playlist
+    sp: spotipy.client.Spotify
+        Spotify client used to download information
+
+    Returns
+    -------
+    final_df: pd.DataFrame
+        Dataframe with features for every song in a playlist
+    """
+    # create a list of song ids
+    ids = []
+
+    for item in results['tracks']['items']:
+        track = item['track']['id']
+        ids.append(track)
+
+    #Guardo de cada cancion los siguientes datos
+    song_meta = {'id': [], 'album': [], 'name': [],
+                 'artist': [], 'explicit': [], 'popularity': []}
+
+    for song_id in ids:
+        # get song's meta data
+        meta = sp.track(song_id)
+
+
+        song_meta['id'].append(meta['id'])
+        song_meta['album'].append(meta['album'])
+        song_meta['name'].append(meta['name'])
+        song_meta['artist'].append(meta['artists'])
+        song_meta['explicit'].append(meta['explicit'])
+        song_meta['popularity'].append(meta['popularity'])        
+        # COMPLETAR: Llenar las listas del diccionario con lo que ven en meta
+
+ 
+    song_meta_df = pd.DataFrame(song_meta) # COMPLETAR: crear un df a partir del diccionario song_meta
+
+    # check the song feature
+    
+    
+    features = sp.audio_features(tracks= ids) # COMPLETAR: Buscar como traer los audio_features a partir de la lista id de song meta
+    # change dictionary to dataframe
+    
+    
+    features_df = pd.DataFrame(features) # COMPLETAR: crear un df a partir del diccionario features
+
+
+    # combine two dataframe
+    final_df = song_meta_df.merge(features_df) # COMPLETAR: mergear features_df y song_meta_df
+    return final_df
+
+
+def download_information(playlist_dict, sp):
+    """
+    Downloads information for all playlists in playlist_dict
+
+    Parameters
+    ----------
+    playlist_dict: dict
+        Dictionary of playlists that we want to download. Each key is the name of the playlist and the values represent
+        the Spotify URI of that playlist
+    sp: spotipy.client.Spotify
+        Spotify client used to download information
+
+    Returns
+    -------
+    df: pd.DataFrame
+        Dataframe with info of all songs of all playlists that were downloaded
+    """
+    # Create an empty output dataframe
+    df = pd.DataFrame()
+
+    # Download information from every playlist
+    for i in playlist_dict.keys():
+        print(f"Downloading information for playlist {i}")
+        df_aux = convert_json_into_df(results=sp.playlist(playlist_dict[i]), sp=sp)
+        df_aux.loc[:, 'Playlist Name'] = i
+        df = pd.concat([df, df_aux], ignore_index=True)
+        print(f"Finished Downloading information for playlist {i}")
+    return df
+
+
+def main():
+    # Connect to API
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # Download results from Top 100 songs from each year
+    my_playlists_id_dict = {
+        1: 'spotify:playlist:37i9dQZEVXbMMy2roB9myp', #TOP 50 ARG
+        2: 'spotify:playlist:4JEwJPzigydrndyf3nsBWg', #Fran Agiro
+        3: 'spotify:playlist:37i9dQZF1DXe1ikyKZnRtc', #This is Duki
+        4: 'spotify:playlist:37i9dQZF1DXbqDp0KLPlRg', #This is Maria Becerra
+        5: 'spotify:playlist:37i9dQZF1DXe0nmj2KyjW1' #This is Tini
+    }
+
+    # Download information from my Top 100 songs by year
+    df_my_songs = download_information(playlist_dict=my_playlists_id_dict, sp=sp)
+    df_my_songs.to_csv('./3 - Spotify Recommender/inputs/my_playlists_info.csv', index=False, encoding='utf-8 sig')
+
+
+if __name__ == '__main__':
+    main()
---
+++ Spotify/c_generate_recommender.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+numeric_variables = ['danceability', 'energy', 'key', 'loudness', 'acousticness', 'instrumentalness', 'liveness', 'valence']
+
+if __name__ == '__main__':
+    # Leer dataframe de sus canciones
+    mis_canciones = pd.read_csv("/workspaces/Taller Coding/Spotify/inputs/my_playlists_info.csv", sep=',') #/3 - Spotify Recommender
+    
+    # Leer dataframe compartido
+    df = pd.read_csv("/workspaces/Taller Coding/Spotify/inputs/kaggle_spotify_data/data.csv", sep = ',')
+    
+    # Crear un diccionario que tenga el promedio de cada variable en numeric_variables viendo sus canciones
+    promedios = dict()
+    
+    for item in numeric_variables:
+        promedios[item] = np.mean(mis_canciones[item])
+    
+    #Imprimir promedios
+    #for x in promedios:
+    #    print(str(x) + ' => ' + str(promedios[x]))
+        
+    # Implementar una funcion que calcule la distancia de cada cancion en el df compartido contra el diccionario que
+    # hicieron
+    df.loc[:, 'Distance'] = 0
+    
+    for item in numeric_variables:
+        df['Distance'] = df['Distance'] + abs(((df[item] - promedios[item])**2)/promedios[item])
+    
+    df['Distance'] = np.sqrt(df['Distance'])
+    
+    # Ver que 30 canciones tienen la menor distancia
+    df = df.sort_values(by=['Distance'], ascending=True).reset_index()
+    print(df)
+    
+    # Guardar el resultado
+    res = df.iloc[:30]
+    print(res)
+    
+    pass
